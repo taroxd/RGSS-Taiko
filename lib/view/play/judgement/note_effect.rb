d0d8bc2374f6ec26cdc5235844b32cef71e9beb7
taroxd <taroxd@163.com>
2015-03-01 18:04:39 +0800

    note_effect.rb: only show normal notes

--- lib/view/play/judgement/note_effect.rb
+++ lib/view/play/judgement/note_effect.rb
@@ -9,6 +9,7 @@ module View
         # ● 重置并显示
         #--------------------------------------------------------------------------
         def reset_and_show(note)
+          return unless note.normal?
           self.bitmap = Cache.note(note)
           @frame = 0
           set_frame
