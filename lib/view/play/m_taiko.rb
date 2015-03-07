ab6492c70f01cfb2166530a041ff581002b57796
taroxd <taroxd@163.com>
2015-03-07 12:22:53 +0800

    refactor view: remove too many folders

--- lib/view/play/m_taiko.rb
+++ lib/view/play/m_taiko.rb
@@ -1,13 +1,26 @@
 # encoding: utf-8
 
-require 'view/play/m_taiko/m_taikoh.rb'
+require 'view/animation'
+require 'view/dispose_bitmap'
 
 module View
   class Play
     class MTaiko
-      #--------------------------------------------------------------------------
-      # ● 初始化
-      #--------------------------------------------------------------------------
+      class MTaikoh < Animation
+        include DisposeBitmap
+        # 精灵的位图由调用者提供
+        def get_bitmap(bitmap)
+          bitmap
+        end
+
+        def show
+          @frame = 0
+          set_frame
+          super
+        end
+      end
+
+      # 初始化
       def initialize(viewport = nil)
         temp_bitmap = Cache.skin("mtaikoflash_red")
         @li = MTaikoh.new(viewport,
@@ -35,9 +48,8 @@ module View
           {x: sf_pos_x, y: sf_pos_y, z: pos_z},
           {filename: Cache.skin("sfieldflash_gogotime"), duration: duration_time})
       end
-      #--------------------------------------------------------------------------
-      # ● 释放
-      #--------------------------------------------------------------------------
+
+      # 释放
       def dispose
         @li.dispose
         @ri.dispose
@@ -47,9 +59,8 @@ module View
         @sfb.dispose
         @sfg.dispose
       end
-      #--------------------------------------------------------------------------
-      # ● 更新
-      #--------------------------------------------------------------------------
+
+      # 更新
       def update
         update_taiko
         update_fumen
@@ -60,18 +71,16 @@ module View
         @sfr.update
         @sfb.update
       end
-      #--------------------------------------------------------------------------
-      # ● 更新太鼓打击特效
-      #--------------------------------------------------------------------------
+
+      # 更新太鼓打击特效
       def update_taiko
         @lo.show if Keyboard.left_outer?
         @ro.show if Keyboard.right_outer?
         @li.show if Keyboard.left_inner?
         @ri.show if Keyboard.right_inner?
       end
-      #--------------------------------------------------------------------------
-      # ● 更新谱面打击特效
-      #--------------------------------------------------------------------------
+
+      # 更新谱面打击特效
       def update_fumen
         if Taiko.gogotime? then @sfg.show
         else
@@ -86,9 +95,8 @@ module View
           end
         end
       end
-      #--------------------------------------------------------------------------
-      # ● 切割图片
-      #--------------------------------------------------------------------------
+
+      # 切割图片
       def get_bitmap(temp_bitmap,type = 0)
         target_bitmap = Bitmap.new(temp_bitmap.width/2,temp_bitmap.height)
         src_rect = Rect.new(temp_bitmap.width/2 * type, 0,
@@ -96,37 +104,30 @@ module View
         target_bitmap.blt(0, 0, temp_bitmap, src_rect)
         return target_bitmap
       end
-      #--------------------------------------------------------------------------
-      # ● Z 坐标
-      #--------------------------------------------------------------------------
+
+      # Z 坐标
       def pos_z; 0; end
-      #--------------------------------------------------------------------------
-      # ● 显示时间
-      #--------------------------------------------------------------------------
+
+      # 显示时间
       def duration_time; 5; end
-      #--------------------------------------------------------------------------
-      # ● 太鼓左侧中心位置
-      #--------------------------------------------------------------------------
+
+      # 太鼓左侧中心位置
       def li_pos_x; SkinSettings.fetch(:MtaikoLIX); end
       def li_pos_y; SkinSettings.fetch(:MtaikoLIY); end
-      #--------------------------------------------------------------------------
-      # ● 太鼓左侧外部位置
-      #--------------------------------------------------------------------------
+
+      # 太鼓左侧外部位置
       def lo_pos_x; SkinSettings.fetch(:MtaikoLOX); end
       def lo_pos_y; SkinSettings.fetch(:MtaikoLOY); end
-      #--------------------------------------------------------------------------
-      # ● 太鼓右侧中心位置
-      #--------------------------------------------------------------------------
+
+      # 太鼓右侧中心位置
       def ri_pos_x; SkinSettings.fetch(:MtaikoRIX); end
       def ri_pos_y; SkinSettings.fetch(:MtaikoRIY); end
-      #--------------------------------------------------------------------------
-      # ● 太鼓右侧外部位置
-      #--------------------------------------------------------------------------
+
+      # 太鼓右侧外部位置
       def ro_pos_x; SkinSettings.fetch(:MtaikoROX); end
       def ro_pos_y; SkinSettings.fetch(:MtaikoROY); end
-      #--------------------------------------------------------------------------
-      # ● 谱面闪烁位置
-      #--------------------------------------------------------------------------
+
+      # 谱面闪烁位置
       def sf_pos_x; SkinSettings.fetch(:MtaikoSFX); end
       def sf_pos_y; SkinSettings.fetch(:MtaikoSFY); end
     end
