8d6c169f0039a94b67d8186647dad09bc952d22e
taroxd <taroxd@163.com>
2015-03-07 13:35:53 +0800

    scene/song_list.rb: save index to file now

--- lib/scene/song_list.rb
+++ lib/scene/song_list.rb
@@ -11,7 +11,7 @@ module Scene
     include Taiko
     include AutoloadView
 
-    @@index = 0
+    INDEX_FILENAME = 'Data/SONGLIST_INDEX'
 
     # 当前曲名
     def songdata(offset = 0)
@@ -24,6 +24,7 @@ module Scene
     def start
       Graphics.resize_screen(480, 272)
       make_song_list
+      load_index
       super
       play_demo
     end
@@ -39,13 +40,32 @@ module Scene
     def update
       super
       update_index
-      update_course(true)  if Input.trigger?(:RIGHT)
-      update_course(false) if Input.trigger?(:LEFT)
+      update_course(1)  if Input.trigger?(:RIGHT)
+      update_course(-1) if Input.trigger?(:LEFT)
       update_scene unless scene_changing?
     end
 
+    # 读取上次的曲子位置
+    def load_index
+      if File.exist?(INDEX_FILENAME)
+        @index, @courses = load_data(INDEX_FILENAME)
+      else
+        @index = 0
+        @courses = Hash.new(0)
+      end
+      @courses.each do |song_index, course|
+        select_course(song_index, course)
+      end
+    end
+
+    # 保存上次的曲子位置
+    def save_index
+      save_data([@index, @courses], INDEX_FILENAME)
+    end
+
+    # 获取取余后的索引
     def songlist_index(offset = 0)
-      (@@index + offset) % @songlist.size
+      (@index + offset) % @songlist.size
     end
 
     def make_song_list
@@ -59,25 +79,40 @@ module Scene
 
     # 更新光标
     def update_index
-      last_index = @@index
+      last_index = @index
       if Input.repeat?(:UP)
-        @@index -= 1
+        @index -= 1
+        @index %= @songlist.size
       elsif Input.repeat?(:DOWN)
-        @@index += 1
+        @index += 1
+        @index %= @songlist.size
       end
-      if last_index != @@index
+      if last_index != @index
         Audio.se_play 'Audio/SE/dong'
         play_demo
+        save_index
       end
     end
 
-    # 更新难度。to_next: 是否向下一难度
-    def update_course(to_next)
-      new_data = to_next ? songdata.next_course : songdata.prev_course
-      if new_data
-        @songlist[songlist_index] = new_data
+    # 更新难度
+    def update_course(course_diff)
+      if select_course(@index, course_diff)
+        @courses[@index] += course_diff
+        save_index
         Audio.se_play 'Audio/SE/dong'
       end
+    end
+
+    # 更改难度。song_index: 歌曲编号。course_diff: 更改的量。
+    # 未发生更改时返回伪值。
+    def select_course(song_index, course_diff)
+      last_data = data = @songlist[song_index]
+      to_next = course_diff > 0
+      course_diff.abs.times do
+        data = (to_next ? data.next_course : data.prev_course) || data
+      end
+      @songlist[song_index] = data
+      !last_data.equal?(data)
     end
 
     # 播放预览
