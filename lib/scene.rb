ef784da4da41634d62119fe05ee1deefed0e316a
taroxd <taroxd@163.com>
2015-03-20 21:22:22 +0800

    scene.rb: simplify the code

--- lib/scene.rb
+++ lib/scene.rb
@@ -1,39 +1,27 @@
-# encoding:utf-8
+# encoding: utf-8
 
 require 'scene/song_list'
 
 module Scene
 
-  @scene = nil                            # 当前场景实例
+  class << self
 
-  # 运行
-  def self.run
-    @scene = first_scene_class.new
-    @scene.main while @scene
-  end
+    attr_reader :scene  # 获取当前场景
 
-  # 获取最初场景的所属类
-  def self.first_scene_class
-    SongList
-  end
+    # 运行
+    def run
+      @scene = first_scene_class.new
+      @scene.main while @scene
+    end
 
-  # 获取当前场景
-  def self.scene
-    @scene
-  end
+    # 获取最初场景的所属类
+    def first_scene_class
+      SongList
+    end
 
-  # 判定当前场景的所属类
-  def self.scene_is?(scene_class)
-    @scene.instance_of?(scene_class)
-  end
-
-  # 直接切换某个场景（无过渡）
-  def self.goto(scene_class)
-    @scene = scene_class.new
-  end
-
-  # 退出游戏
-  def self.exit
-    @scene = nil
+    # 切换某个场景
+    def goto(scene_class)
+      @scene = scene_class.new
+    end
   end
 end
